ad88976b94eb71494a58c273a10e4f58e65eb8c7
codemode9 <timourbasegmez@hotmail.com>
2018-02-08 21:08:40 -0500

    is now a psychic game and not a hangman game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,24 @@
-var words = ['werewolf', 'drakula', 'frankenstein', 'zombie'];
-var word = words(Math.floor(Math.random()*words.length));
-// ^perhaps array
-var playerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var choices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var wins = 0;
 var losses = 0;
-var guessesLives = 0;
+var lives = 10;
+var compChoice = choices[Math.floor(Math.random()*choices.length)];
 
-document.onKeyUp = function(event) {
-    var playerLetter = event.key;
-    
-}
+document.addEventListener("keyup", function(event) {
+    var playerChoice = event.key;
+    if (playerChoice === compChoice) {
+        wins++;
+    }
+    else {
+        lives--;
+    }
+    if(lives === 0) {
+        losses++;
+    }
+    if (lives < 1) {
+       lives += 10;
+    }
+    document.getElementById("wins").innerHTML = "Wins: " + wins;
+    document.getElementById("losses").innerHTML = "Losses: " + losses;
+    document.getElementById("lives").innerHTML = "Lives: " + lives;
+})
